7b6737d1bcc6f77e1d2de50ef31083919d43ecf3
Eli Morris <elimorris@Elis-MBP.lan>
2023-04-27 22:32:42 -0700

    intitial commit(file setup)

---
+++ server.js
@@ -0,0 +1,6 @@
+//const express = require('express');
+//const routes = require('./Controllers');
+//const path = require('path');
+//const sequelize = require('./Config/connection');
+
+//const helpers = require('./utils/helpers');
