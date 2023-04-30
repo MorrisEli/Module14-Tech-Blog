19cfcdafaf66de6910925d02605b95fbfc401b6e
Eli Morris <elimorris@Elis-MBP.lan>
2023-04-30 12:59:24 -0700

    reconstructed connection and server file, added more files

--- server.js
+++ server.js
@@ -1,6 +1,42 @@
-//const express = require('express');
-//const routes = require('./Controllers');
-//const path = require('path');
-//const sequelize = require('./Config/connection');
+const express = require('express');
+const routes = require("./Controllers");
+const sequelize = require("./Config/connection");
+const exphbs = require("express-handlebars");
+const helpers = require("./utils/helpers");
+const hbs = exphbs.create({ helpers });
+const session = require("express-session");
+const path = require('path');
+require('dotenv').config();
 
-//const helpers = require('./utils/helpers');
+const SequalizeStore = require("connect-session-sequilize")(session.Store);
+
+const sess = {
+    secret: process.env.Secret,
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequalizeStore({
+        db: sequelize,
+    }),
+};
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(session(sess));
+
+app.use(routes);
+
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
+
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen( PORT, () => 
+    console.log(`You're now listening on port: ${PORT}`)
+    );
+});
